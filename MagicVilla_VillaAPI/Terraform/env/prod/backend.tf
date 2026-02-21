6b3d2a2450334f8770e27317faa1f6a9669dc21c
Alshaymaa Hamdan <alshaymaa.aref@hotmail.com>
2026-02-21 23:44:38 +0300

    remove var from backed.tf

--- MagicVilla_VillaAPI/Terraform/env/prod/backend.tf
+++ MagicVilla_VillaAPI/Terraform/env/prod/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "tf-state-shaymaa"  # your S3 bucket
     key            = "prod/terraform.tfstate"     # path inside the bucket
-    region         = var.region
+    region         = "us-west-2"
     dynamodb_table = "tf-lock-alshaymaa"            # DynamoDB table for locking
     encrypt        = true                          # encrypt state at rest
   }
