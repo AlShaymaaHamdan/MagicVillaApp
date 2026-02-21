aa4bb0f7f1169271528ab69914ab00a110088cd5
Alshaymaa Hamdan <alshaymaa.aref@hotmail.com>
2026-02-22 00:10:06 +0300

    edit key in backend.tf

--- MagicVilla_VillaAPI/Terraform/env/prod/backend.tf
+++ MagicVilla_VillaAPI/Terraform/env/prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "tf-state-shaymaa"  # your S3 bucket
-    key            = "/envprod/terraform.tfstate"     # path inside the bucket
+    key            = "prod/terraform.tfstate"     # path inside the bucket
     region         = "us-west-2"
     dynamodb_table = "tf-lock-alshaymaa"            # DynamoDB table for locking
     encrypt        = true                          # encrypt state at rest
