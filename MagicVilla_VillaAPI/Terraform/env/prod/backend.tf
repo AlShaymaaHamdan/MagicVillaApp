3023fcdec73efa9e1e702af141a928bda52ab4f6
Alshaymaa Hamdan <alshaymaa.aref@hotmail.com>
2026-03-02 03:19:52 +0300

    edit backend

--- MagicVilla_VillaAPI/Terraform/env/prod/backend.tf
+++ MagicVilla_VillaAPI/Terraform/env/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-state-shaymaa"  # your S3 bucket
+    bucket         = "alshaymaahamdan"  # your S3 bucket
     key            = "prod/terraform.tfstate"     # path inside the bucket
     region         = "us-west-2"
     dynamodb_table = "tf-lock-alshaymaa"            # DynamoDB table for locking
